44acdd5cc03d4153cae95924b367a716d5d76835
KSK-engineer <mutekifield@yahoo.co.jp>
2020-05-24 20:16:30 +0900

    repair to 'group section'

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  has_many :group_user       
+  has_many :group_user
   has_many :group, through: :group_user
   has_many :messages
 end
